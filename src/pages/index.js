736eaa7d1dee5d7a8fc8f99b00ae6af9d581b8f4
ajerni <ajerni@gmail.com>
2019-08-27 01:18:58 +0200

    added state with hook useState and toggle img

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import { Link } from "gatsby"
 import { Button } from "react-bootstrap"
 
@@ -8,9 +8,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => {
 
-    let clickHandler = () => {
-      console.log(console.log("click works"))
-    };
+  const [toggle, setToggle] = useState(true);
   
  return( 
   <Layout>
@@ -19,16 +17,16 @@ const IndexPage = () => {
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    
+    {toggle ?
       <Image />
-      
+      :
       <img
         src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
         alt="Group of pandas eating bamboo"
       />
-    
+    }
     </div>
-    <Button onClick={clickHandler} style={{margin: 20}} variant="success">nice react bootsrap button</Button>
+    <Button onClick={() => setToggle(!toggle)} style={{margin: 20}} variant="success">nice react bootsrap button</Button>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )}
