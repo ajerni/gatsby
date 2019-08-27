460a7ce3db6f0ade6ba1277425d09fe0ca78c5ac
ajerni <ajerni@gmail.com>
2019-08-27 10:58:34 +0200

    changed png file

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Button } from "react-bootstrap"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
+import coin from "../images/coin.png"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
@@ -20,10 +21,7 @@ const IndexPage = () => {
     {toggle ?
       <Image />
       :
-      <img
-        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-        alt="Group of pandas eating bamboo"
-      />
+      <img src={coin} alt="coin"/>
     }
     </div>
     <Button onClick={() => setToggle(!toggle)} style={{margin: 20}} variant="success">nice react bootsrap button</Button>
