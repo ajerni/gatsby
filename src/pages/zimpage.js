d94968f7f8d43b418b465eaa00b2aa47faf320d4
ajerni <ajerni@gmail.com>
2019-08-27 13:58:29 +0200

    added zim and commented out again

---
+++ src/pages/zimpage.js
@@ -0,0 +1,14 @@
+// import React from "react"
+// import { Link } from "gatsby"
+
+// import Layout from "../components/layout"
+// import ZIM from "../components/zimcomponent"
+// import SEO from "../components/seo"
+
+// const ZimPage = () => (
+//   <Layout>
+//     <ZIM></ZIM>
+//   </Layout>
+// )
+
+// export default ZimPage
