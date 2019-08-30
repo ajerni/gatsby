7eb4ce38658f8c361b5dc1b5f266b1b095274860
ajerni <ajerni@gmail.com>
2019-08-30 16:27:29 +0200

    form still working?

--- src/pages/formpage.js
+++ src/pages/formpage.js
@@ -8,15 +8,9 @@ const FormPage = () => (
   <Layout>
     <SEO title="form page" />
     <div>
-      <form
-        name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        {" "}
-        <input type="hidden" name="bot-field" />{" "}
-        <input type="hidden" name="form-name" value="contact" />{" "}
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
         <div className="field half first">
           <label htmlFor="name">Name</label>
           <input type="text" name="name" id="name" />
