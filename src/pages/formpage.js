7190b71d5999e6acdcf0e466d6543de23e5bf7bd
ajerni <ajerni@gmail.com>
2019-08-30 15:44:01 +0200

    form changed

--- src/pages/formpage.js
+++ src/pages/formpage.js
@@ -8,8 +8,7 @@ const FormPage = () => (
   <Layout>
     <SEO title="Page two" />
     <div>
-    <form name="contact" method="post" netlify-honeypot="bot-field" data-netlify="true">
-    <input type="hidden" name="bot-field" />
+    <form name="contact" method="POST" data-netlify="true">
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
   </p>
