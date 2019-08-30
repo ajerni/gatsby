4ab1ee47d5d9b11deb4e5a265da1cdcaab925b1c
ajerni <ajerni@gmail.com>
2019-08-30 16:12:45 +0200

    form again...

--- src/pages/formpage.js
+++ src/pages/formpage.js
@@ -8,25 +8,34 @@ const FormPage = () => (
   <Layout>
     <SEO title="Page two" />
     <div>
-    <form name="kontaktform" method="post" data-netlify="true" action="/pages/index">
-   
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-    <br></br>
-
+      <form
+        name="kontaktform"
+        method="post"
+        data-netlify="true"
+        action="/index"
+      >
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+      <br></br>
     </div>
-    
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
