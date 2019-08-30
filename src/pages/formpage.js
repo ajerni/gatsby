21ccb85e6255f4a39c1610f125e18e935c389f57
ajerni <ajerni@gmail.com>
2019-08-30 16:09:12 +0200

    kontaktform

--- src/pages/formpage.js
+++ src/pages/formpage.js
@@ -8,8 +8,8 @@ const FormPage = () => (
   <Layout>
     <SEO title="Page two" />
     <div>
-    <form name="kontakt" method="post" netlify-honeypot="bot-field" data-netlify="true" action="/pages/index">
-   <input type="hidden" name="bot-field" />
+    <form name="kontaktform" method="post" data-netlify="true" action="/pages/index">
+   
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
   </p>
