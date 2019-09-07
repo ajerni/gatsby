11913bbf5d2c896e46cacca11a0b167bbd5662e8
ajerni <ajerni@gmail.com>
2019-09-07 09:58:11 +0200

    make selection work / loop error

--- src/pages/datapage3.js
+++ src/pages/datapage3.js
@@ -9,31 +9,6 @@ import SEO from "../components/seo"
 
 const DataPage3 = () => {
 
-  const APOLLO_QUERY3 = gql`
-  query GetImage($id : ID){
-  character(id: $id) {
-    id
-    name
-    image
-  }
-}
-`
-
-  const [selectedId, setSelectedId] = useState("89")
-  const [getPhoto, { loading, error, data }] = useLazyQuery(APOLLO_QUERY3);
-
-//should this be within useEffec hook?
-
-  // function val(selection) {
-  //   setSelectedId(selection)
-  //   console.log(selectedId)
-  // }
-  //const val = () => {}
-  //setSelectedId("cjke2xlf9nhd90953khilyzja")
-  
-  
-
-  
   // This query is executed at build time by Gatsby. (and refetched by refetchInterval in gatsby.config.js)
   const mydata = useStaticQuery(graphql`
     query MyQuery3 {
@@ -51,13 +26,25 @@ const DataPage3 = () => {
     }
   `)
 
-  // This query is executed at run time by Apollo.
-  //TODO: id: "xyz" should be taken from the dropdown (probably already in state selectedID)
-  
+  // This query is executed at run time by Apollo client (onClick of button "Get Data")
+  const APOLLO_QUERY3 = gql`
+  query GetImage($id : ID){
+  character(id: $id) {
+    id
+    name
+    image
+  }
+}
+`
 
-  //const { loading, error, data } = useQuery(APOLLO_QUERY3) //this should happen below on press of the button 'Get Data'
+  const [selectedId, setSelectedId] = useState("89")
+  const [getPhoto, { loading, error, data }] = useLazyQuery(APOLLO_QUERY3);
 
-  //TODO: make APOLLO_QUERY on press of button and update page (id: in the query must be provided from the client by form or dropdown or textfield etc.)
+//ERROR: Too many re-renders. React limits the number of renders to prevent an infinite loop.
+  function val(selection) {
+    setSelectedId(selection)
+    console.log(selectedId)
+  }
   
   return (
     <Layout>
@@ -69,16 +56,17 @@ const DataPage3 = () => {
         <p key={user.id}>{user.name}</p>
       ))}
       <br></br>
-      <h2>Data from public API on graphqlcms</h2>
-      <h3>at run-time --> useQuery (Apollo Client):</h3>
-      <select id="select_id">
-      {/* <select onChange={val(this)} id="select_id"> */}
+      <h2>Data from public API</h2>
+      <h3>at run-time --> useQuery/useLazyQuery (Apollo Client):</h3>
+      {/* TODO: setSelectedId to selection onChange */}
+      {/* <select id="select_id"> */}
+      <select onChange={val(this)} id="select_id">
       {/* <select onChange={(this)=>setSelectedId(this))} id="select_id"> */}
         <option value="">Select id:</option>
-        <option value="cjke2us3lng6v0953owd9fp15">Bild 1</option>
-        <option value="cjke2vvfdngi70953ia4b4156">Bild 2</option>
-        <option value="cjuk1n87kbmio0c15x0q0iyy0">Bild 3</option>
-        <option value="cjke2xlf9nhd90953khilyzja">Bild 4</option>
+        <option value="89">Bild 1</option>
+        <option value="91">Bild 2</option>
+        <option value="92">Bild 3</option>
+        <option value="123">Bild 4</option>
       </select>
 
       {loading && <p>Loading image...</p>}
