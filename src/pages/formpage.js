7d96c0ab3677831ebdfaf26869e7a4b1f52289fc
ajerni <ajerni@gmail.com>
2019-09-08 11:54:08 +0200

    final v1 with new formpage

--- src/pages/formpage.js
+++ src/pages/formpage.js
@@ -1,42 +1,36 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Form, Button} from "react-bootstrap"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const FormPage = () => (
   <Layout>
-    <SEO title="form page" />
     <div>
-      <form
-        name="contact"
+      <Form
+        name="contactform"
         method="post"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
         <input type="hidden" name="bot-field" />
-        <input type="hidden" name="form-name" value="contact" />
-        <div className="field half first">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
-        </div>
-        <div className="field half">
-          <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" />
-        </div>
-        <div className="field">
-          <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" rows="6" />
-        </div>
-        <ul className="actions">
-          <li>
-            <input type="submit" value="Send Message" className="special" />
-          </li>
-          <li>
-            <input type="reset" value="Clear" />
-          </li>
-        </ul>
-      </form>
+        <input type="hidden" name="form-name" value="contactform" />
+        <Form.Group controlId="contactForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" name="email" id="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group controlId="contactForm.ControlInput2">
+          <Form.Label>Subject</Form.Label>
+          <Form.Control type="text" name="subject" id="subject" />
+        </Form.Group>
+        <Form.Group controlId="contactForm.ControlInput2">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows="3" name="message" id="message"/>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
     <br></br>
     <Link to="/">Go back to the homepage</Link>
