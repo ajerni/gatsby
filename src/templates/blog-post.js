bff474d8560600d463c2db103d9f3332d0193f0f
ajerni <ajerni@gmail.com>
2019-09-09 00:05:42 +0200

    cosmetics 2

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -13,7 +13,9 @@ export default ({ data }) => {
         </div>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         <br></br>
+        <Link to="/markdownpage" style={{marginRight: 20}}>Go back to posts</Link>
         <Link to="/">Go back to homepage</Link>
+        <div style={{marginBottom:20}}></div>
       </div>
     </Layout>
   )
