13a8b03b6d436a7ae65d7f6d18aa1340b4d6ad99
ajerni <ajerni@gmail.com>
2019-09-09 15:21:08 +0200

    pagination /blog /blog/2

--- gatsby-node.js
+++ gatsby-node.js
@@ -29,14 +29,32 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
+  // Create single pages per post (= per markdown file)
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
       component: path.resolve(`./src/templates/blog-post.js`),
       context: {
         // Data passed to context is available
-        // in page queries as GraphQL variables.
+        // in page queries as GraphQL variables. See templates!
         slug: node.fields.slug,
+      },
+    })
+  })
+
+  // Create blog-list pages (Pagination)
+  const posts = result.data.allMarkdownRemark.edges
+  const postsPerPage = 2
+  const numPages = Math.ceil(posts.length / postsPerPage)
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+      component: path.resolve(`./src/templates/blog-list-template.js`),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
       },
     })
   })
