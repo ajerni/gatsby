0a59141dd574b69bff0605264c9a691752ce424c
ajerni <ajerni@gmail.com>
2019-09-10 18:39:42 +0200

    added Lambda function hello-world

---
+++ functions/hello-world/hello-world.js
@@ -0,0 +1,11 @@
+exports.handler = async (event, context) => {
+    try {
+      const subject = event.queryStringParameters.name || "World"
+      console.log(event)
+      return { statusCode: 200, body: `Hello ${subject}` }
+    } catch (err) {
+      return { statusCode: 500, body: err.toString() }
+    }
+  }
+  
+  
